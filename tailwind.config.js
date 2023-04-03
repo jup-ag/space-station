ab1ab1c805461fb514e7270aaadf0d81f421b3a3
Dong Nguyen <dongnguyen9186@gmail.com>
2023-04-03 21:50:22 +0700

    add new homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', '../docs/**/*.mdx'], // my markdown stuff is in ../docs, not /src
   darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        content: 1216,
+      },
+    },
   },
   plugins: [],
 };
