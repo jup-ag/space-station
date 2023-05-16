13b7985ae9b281b49011c4172504d6d47d41ff0b
mpng <meijiao@gmail.com>
2023-05-16 10:01:35 +0800

    put og back

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,6 +112,14 @@ const config = {
           content:
             "Jupiter Station is the place for all things related to Jupiter. Here you can access comprehensive documentation, explore statistics and analytics, ecosystem partners that are integrating with Jupiter and read our latest updates.",
         },
+        {
+          property: 'og:image',
+          content: 'https://og.jup.ag/api/jupiter-station',
+        },
+        {
+          property: 'theme-color',
+          content: '#000000',
+        }
       ],
       colorMode: {
         defaultMode: "light",
