2952225835e48909d4af057772ceddb4ed50f097
Billy Jacoby <billyjacoby@gmail.com>
2024-05-30 13:48:16 -0400

    style: update mega menu styles

--- src/components/MegaMenu/MegaMenu.tsx
+++ src/components/MegaMenu/MegaMenu.tsx
@@ -16,41 +16,45 @@ import {
 import NavbarItem from '@theme/NavbarItem';
 import { ChevronDown } from 'lucide-react';
 
-const components: { title: string; href: string; description: string }[] = [
+type LinkGroup = {
+  heading: string;
+  children: { title: string; href: string }[];
+};
+
+const linkItems: LinkGroup[] = [
   {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
+    heading: 'Products',
+    children: [
+      {
+        title: 'Alert Dialog',
+        href: '/docs/primitives/alert-dialog',
+      },
+      {
+        title: 'Hover Card',
+        href: '/docs/primitives/hover-card',
+      },
+      {
+        title: 'Progress',
+        href: '/docs/primitives/progress',
+      },
+    ],
   },
   {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+    heading: 'APIs',
+    children: [
+      {
+        title: 'Scroll-area',
+        href: '/docs/primitives/scroll-area',
+      },
+      {
+        title: 'Tabs',
+        href: '/docs/primitives/tabs',
+      },
+      {
+        title: 'Tooltip',
+        href: '/docs/primitives/tooltip',
+      },
+    ],
   },
 ];
 
@@ -75,7 +79,11 @@ export function MegaMenu() {
             </div>
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="border-4 border-white grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
+            <div className="border-4 border-white grid gap-3 p-6 border-solid">
+              <span className="text-white text-lg font-bold ">Products</span>
+              <span className="w-full border-b border-[#00BCF0] border-solid border-t-transparent border-x-transparent" />
+            </div>
+            <ul className="border-4 border-white grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3 ">
                 <NavigationMenuLink asChild>
                   <a
@@ -118,26 +126,26 @@ export function MegaMenu() {
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
+              {linkItems.map((linkGroup) => (
+                <>
+                  <div className="grid gap-3">
+                    <span className="text-white text-lg font-bold -mb-2">
+                      Products
+                    </span>
+                    <span className="w-full border-b-[0.5px] border-[#00BCF090] border-solid border-t-transparent border-x-transparent" />
+                  </div>
+                  {linkGroup.children.map((component) => (
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      href={component.href}
+                    />
+                  ))}
+                </>
               ))}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        {/* <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            href="/docs"
-          >
-            Documentation
-          </NavigationMenuLink>
-        </NavigationMenuItem> */}
       </NavigationMenuList>
     </NavigationMenu>
   );
@@ -153,12 +161,12 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground text-white',
+            'block select-none space-y-1 rounded-md leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground text-[#56BBEB] font-bold',
             className
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
+          <div className="text-sm leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
--- src/components/MegaMenu/Menu.tsx
+++ src/components/MegaMenu/Menu.tsx
@@ -70,7 +70,7 @@ const NavigationMenuContent = React.forwardRef<
     ref={ref}
     className={cn(
       'left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ',
-      'bg-[#00BCF020] backdrop-blur p-4 border-4 border-white',
+      'bg-[#00BCF020] backdrop-blur p-4 border-solid border border-[#00BCF090] rounded-lg',
       className
     )}
     {...props}
