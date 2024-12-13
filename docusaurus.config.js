062697d79981665374c7b46af477c4976854798e
YY <yy@raccoons.dev>
2024-12-13 19:08:06 +0800

    add awesomeqa script in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -315,6 +315,33 @@ const config = {
         additionalLanguages: ["rust"],
       },
     }),
+
+  scripts: [
+    {
+      id: "awesomeqa-chat-widget-script",
+      src: "https://awesomeqa.xyz/web-support/chat-widget.js",
+      async: true,
+      communityId: "0e700188-8410-431a-b2de-b23cc4ba7308",
+      config: JSON.stringify({
+        theme: "dark",
+        widgetTitle: "Station Chatbot",
+        firstMessage: "Hello there, how can we help you?\n\nWe appreciate your patience! If you require further assistance, please use the buttons below.",
+        primaryColor: "#20b2aa",
+        disclaimer: "We are experimenting with the AI Chatbot and it will do its best to answer.",
+        actions:
+          {
+            contactSupport: {
+              title: "Ticket Support",
+              response: "Thanks for choosing to contact our support, please sit tight and we'll be with you soon.",
+            },
+            thanks: {
+              title: "Thanks",
+              response: "Thanks for your patience and understanding, hope you enjoy using Jupiter and looking forward to more feedback! Test [Discord](https://discord.gg/jup)"
+            }
+          }
+      }),
+    },
+  ],
 };
 
 module.exports = config;
