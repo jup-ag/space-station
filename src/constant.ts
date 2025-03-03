de71d703bafa9e7ff8fcc802e39d8965a7888a3f
YY <yy@raccoons.dev>
2025-03-03 13:50:57 +0800

    add ultra api to navbar

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,7 @@
 export const navbarConfigs = {
     '/docs': [
       { to: '/docs/', label: 'API Setup', position: 'left' },
+      { to: '/docs/ultra-api/', label: 'Ultra API', position: 'left' },
       { to: '/docs/swap-api/get-quote', label: 'Swap API', position: 'left' },
       { to: '/docs/perp-api/', label: 'Perp API', position: 'left' },
       { to: '/docs/tool-kits/swap-terminal', label: 'Tool Kits', position: 'left' },
