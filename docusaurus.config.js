20e5cb67ae44c7b1e848c038adaa072e3da5647a
YY <yy@raccoons.dev>
2025-03-26 22:46:57 +0800

    merge main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,9 +9,9 @@ require("dotenv").config();
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Jupiter Developer",
+  title: "Jupiter Developer Docs",
   staticDirectories: ["static"],
-  tagline: "The Key Liquidity Aggregator and Swap Infrastructure for Solana",
+  tagline: "Build with the most comprehensive APIs and no-code tool kits powered by Jupiter",
   favicon: "img/favicon.ico",
   customFields: {
     // Put your custom environment here
@@ -25,7 +25,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Raccoons", // Usually your GitHub org/user name.
-  projectName: "Jupiter Space Station", // Usually your repo name.
+  projectName: "Jupiter Developer Docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
@@ -61,7 +61,7 @@ const config = {
     //       // Please change this to your repo.
     //       // Remove this to remove the "edit this page" links.
     //       sidebarCollapsed: false,
-    //       editUrl: "https://github.com/jup-ag/space-station/tree/main/",
+    //       editUrl: "https://github.com/jup-ag/docs/tree/main/",
     //     },
 
     //     blog: {
@@ -100,7 +100,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars-docs.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/jup-ag/space-station/tree/main/",
+          editUrl: "https://github.com/jup-ag/docs/tree/main/",
           // docLayoutComponent: "@theme/DocPage",
           docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi-docs
         },
@@ -126,7 +126,7 @@ const config = {
         routeBasePath: "guides",
         sidebarPath: require.resolve("./sidebars-guides.js"),
         sidebarCollapsed: true,
-        editUrl: "https://github.com/jup-ag/space-station/tree/main/",
+        editUrl: "https://github.com/jup-ag/docs/tree/main/",
       }),
     ],
     [
@@ -185,7 +185,7 @@ const config = {
         {
           property: "description",
           content:
-            "Jupiter Developer is the place for all things related to Jupiter. Here you can access comprehensive documentation, explore statistics and analytics, ecosystem partners that are integrating with Jupiter and read our latest updates.",
+            "Jupiter Developer Docs provides comprehensive documentation, tool kits and references for developing with the Jupiter API.",
         },
         {
           property: "og:image",
@@ -227,13 +227,6 @@ const config = {
         theme: lightTheme,
         darkTheme: darkTheme,
         additionalLanguages: ["bash", "json", "rust"],
-      },
-      announcementBar: {
-        id: 'zendesk-migration',
-        content: 'We are migrating our User Guides from Station to our new Support System. Please visit <a target="_blank" rel="noopener noreferrer" href="https://jupiverse.zendesk.com/hc/en-us">Jupiter Helpdesk</a> for the latest guides.',
-        backgroundColor: '#FFA500',
-        textColor: '#000000',
-        isCloseable: false,
       },
       languageTabs: [
         {
